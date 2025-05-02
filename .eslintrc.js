3e49b8f8f88627838a65f43d1124a10e255eeb2b
Jess Chadwick <jesschadwick@gmail.com>
2025-05-01 23:24:59 -0400

    build: update project configuration and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,29 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
+    project: ['./tsconfig.json', './frontend/tsconfig.json', './backend/tsconfig.json'],
   },
   rules: {
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
   overrides: [
     {
-      files: ['**/*.test.js', '**/*.spec.js'],
+      files: ['**/*.test.ts', '**/*.spec.ts', '**/*.test.tsx', '**/*.spec.tsx'],
       env: {
         jest: true,
       },
