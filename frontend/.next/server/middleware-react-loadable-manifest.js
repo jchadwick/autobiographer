98115646078123301fe37d1b9d95c53e3083a10d
Jess Chadwick <jesschadwick@gmail.com>
2025-05-02 00:37:52 -0400

    chore: remove obsolete Next.js build artifacts and cache files

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST =
-  '{"components/TextEditor.tsx -> react-quill":{"id":1167,"files":["static/chunks/eabe11fc.127f806253a0cec7.js","static/chunks/167.2ce881da2189712f.js"]}}';
